cd3f0897d38f1840efdda07f723d482a16b18d11
namrutha <namruthabv12@gmail.com>
2018-07-28 09:03:38 -0700

    added controllers and models

---
+++ rblog_html/rBlog/app/controllers/admin/users_controller.rb
@@ -0,0 +1,22 @@
+class Admin::UsersController < ApplicationController
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+end
